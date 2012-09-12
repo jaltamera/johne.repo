f17e31df9817f743b794080c5c726c76420392e3
Johne <jaltamera@exakt.com.ph>
2012-09-12 10:32:40 +0800

    AA_Processor_Prod 2.2

---
+++ AA/src/aa/exception/AAException.java
@@ -0,0 +1,28 @@
+package aa.exception;
+
+public class AAException extends Exception {
+	
+	String mistake;
+	//----------------------------------------------
+	// Default constructor - initializes instance variable to unknown
+	public AAException()
+	{
+		super();             // call superclass constructor
+		mistake = "Error connecting to AA";
+	}
+
+	//-----------------------------------------------
+	// Constructor receives some kind of message that is saved in an instance variable.
+	public AAException(String err)
+	{
+		super(err);     // call super class constructor
+		mistake = err;  // save message
+	}
+
+	//------------------------------------------------  
+	// public method, callable by exception catcher. It returns the error message.
+	public String getError()
+	{
+		return mistake;
+	}
+}
---
+++ AA/src/aa/exception/IGException.java
@@ -0,0 +1,28 @@
+package aa.exception;
+
+public class IGException extends Exception {
+	
+	String mistake;
+	//----------------------------------------------
+	// Default constructor - initializes instance variable to unknown
+	public IGException()
+	{
+		super();             // call superclass constructor
+		mistake = "Error connecting to IG";
+	}
+
+	//-----------------------------------------------
+	// Constructor receives some kind of message that is saved in an instance variable.
+	public IGException(String err)
+	{
+		super(err);     // call super class constructor
+		mistake = err;  // save message
+	}
+
+	//------------------------------------------------  
+	// public method, callable by exception catcher. It returns the error message.
+	public String getError()
+	{
+		return mistake;
+	}
+}
